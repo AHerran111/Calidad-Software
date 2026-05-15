5ce8cc64b4a7e23074c9df8311b0df8e43670386
AHerran111 <166190104+AHerran111@users.noreply.github.com>
2026-05-15 16:38:56 +0000

    fiexd

--- utils/server_control.py
+++ utils/server_control.py
@@ -5,7 +5,7 @@ import os
 
 def load_token():
     try:
-        return dict(st.secrets["do_token"])
+        return dict(st.secrets["do_token"])["token"]
     
     except Exception:
         secrets_path = os.path.dirname(__file__)+"/secrets.toml"
@@ -16,7 +16,7 @@ def load_token():
         return config["do_token"]["token"]
 
 DO_TOKEN = load_token()
-
+print(DO_TOKEN)
 
 DROPLETS = {
     "postgres_cliente": "570747060",
